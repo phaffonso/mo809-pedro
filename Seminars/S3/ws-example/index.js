a951249ac0da8796e93f17b97831506605eeb49b
phaffonso <pedrohaffonso@gmail.com>
2016-10-11 14:28:43 -0300

    exemplos seminario 3

---
+++ Seminars/S3/ws-example/index.js
@@ -0,0 +1,21 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
+
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ port: 8080 , path: "/echo"});
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
